9d299b1da7046eb0784abbdf133e3ac048604fdb
Anurag Deol <anuragdeol2017@gmail.com>
2022-09-02 21:32:55 +0530

    show list of PROJECT(s) and add new PROJECT

--- controllers/projects_controller.js
+++ controllers/projects_controller.js
@@ -2,9 +2,9 @@ const Project = require('../models/project');
 
 module.exports.create = async function(req, res) {
     await Project.create({
-        name: req.name,
-        author: req.author,
-        description: req.description
+        name: req.body.name,
+        author: req.body.author,
+        description: req.body.description
     });
 
     return res.redirect('back');
--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const db = require('./config/mongoose');
-const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
 // const session = require('express-session');
 
 
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 
 // view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// middleware to parse the request
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', require('./routes'));
 
